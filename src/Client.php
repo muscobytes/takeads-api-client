5111655706ef028d9a8d794a9e9f9502b166d486
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-02 21:53:55 +0400

    feat(client): add `JsonMiddleware` to request middlewares
    - Add `JsonMiddleware` import to `Client.php`
    - Include `JsonMiddleware` in the middlewares array within the `call` method
    - Ensure `JsonMiddleware` is initialized with the request payload as an array
    - Improve code organization by rearranging method positions in `Client.php`

--- src/Client.php
+++ src/Client.php
@@ -7,6 +7,7 @@ use Generator;
 use Muscobytes\HttpClient\Interface\HttpClientInterface;
 use Muscobytes\HttpClient\Middleware\Authentication\BearerMiddleware;
 use Muscobytes\HttpClient\Middleware\ContentTypeMiddleware;
+use Muscobytes\HttpClient\Middleware\Payload\JsonMiddleware;
 use Muscobytes\TakeAdsApi\Dto\RequestDto;
 use Muscobytes\TakeAdsApi\Dto\ResponseDto;
 use Muscobytes\TakeAdsApi\Factory\HttpClientFactory;
@@ -24,20 +25,22 @@ class Client
         if (!$this->client) {
             $this->client = HttpClientFactory::create();
         }
-        $this->middlewares = [
-            new BearerMiddleware($this->publicKey),
-            new ContentTypeMiddleware('application/json')
-        ];
     }
 
 
     public function call(RequestDto $request): ResponseDto|Generator
     {
+        $this->middlewares = [
+            new BearerMiddleware($this->publicKey),
+            new ContentTypeMiddleware('application/json'),
+            new JsonMiddleware($request->toArray())
+        ];
+
         return $request->makeResponseDto(
             $this->client->request(
                 $request->getHttpMethod(),
                 $this->base_uri . $request->getUrlPath(),
-                $this->middlewares
+                $this->middlewares,
             )
         );
     }
