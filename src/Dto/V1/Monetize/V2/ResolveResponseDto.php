314a3ceb9024f0e4b8cdd5f89fdb3df87eb2e686
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-02 20:05:08 +0400

    style(dto): reformat docstring for better readability
    - Split long docstring line for `$trackingLink` parameter
    - Improve readability by aligning description text

--- src/Dto/V1/Monetize/V2/ResolveResponseDto.php
+++ src/Dto/V1/Monetize/V2/ResolveResponseDto.php
@@ -14,7 +14,8 @@ class ResolveResponseDto extends ResponseDto
 {
     /**
      * @param string $iri Link sent in the request.
-     * @param string $trackingLink Affiliate link to the advertiser's website (RFC 3986). You can update the s (SubID) and url (Deeplink) parameters after the tracking link is generated.
+     * @param string $trackingLink Affiliate link to the advertiser's website (RFC 3986).
+     *      You can update the s (SubID) and url (Deeplink) parameters after the tracking link is generated.
      * @param string|null $imageUrl Link to the advertiser's logo, if requested (RFC 3986).
      */
     public function __construct(
