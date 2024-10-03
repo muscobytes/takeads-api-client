7793974912d528a7b4451e383ac517e9803ab0ae
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-03 13:15:26 +0400

    feat(merchant): add `MerchantRequest` class for fetching merchant list
    - Introduce `MerchantRequest` class in `src/Dto/V1/Monetize/V2/Merchant/MerchantRequest.php`
    - Implement functionality to get a list of merchants as per API documentation
    - Utilize `Get` trait for HTTP GET requests
    - Define `makeResponse` method to handle API response and return `MerchantResponse`

---
+++ src/Dto/V1/Monetize/V2/Merchant/MerchantRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\Merchant;
+
+use Muscobytes\TakeAdsApi\Dto\Request;
+use Muscobytes\TakeAdsApi\Dto\Response;
+use Muscobytes\TakeAdsApi\Traits\Get;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Get list of merchants
+ * https://developers.takeads.com/knowledge-base/article/get-list-of-merchants
+ */
+class MerchantRequest extends Request
+{
+    use Get;
+
+    protected string $path = '/v1/product/monetize-api/v2/merchant';
+
+    public function makeResponse(ResponseInterface $response): Response
+    {
+        return new MerchantResponse($response);
+    }
+}
