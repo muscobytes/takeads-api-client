8f4a349e063fab252a99497187012a6c6027ffda
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-03 13:29:52 +0400

    feat(merchant): add `MerchantResponse` class for merchant data handling
    - Introduce `MerchantResponse` class in `src/Dto/V1/Monetize/V2/Merchant`
    - Implement `getData` method to yield `MerchantDto` instances from API response
    - Utilize `HasMeta` trait for metadata management
    - Enhance API client's ability to process merchant data efficiently

---
+++ src/Dto/V1/Monetize/V2/Merchant/MerchantResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\Merchant;
+
+use Generator;
+use Muscobytes\TakeAdsApi\Dto\Response;
+use Muscobytes\TakeAdsApi\Traits\HasMeta;
+
+/**
+ * Get list of merchants
+ * https://developers.takeads.com/knowledge-base/article/get-list-of-merchants
+ */
+class MerchantResponse extends Response
+{
+    use HasMeta;
+
+    public function getData(): Generator
+    {
+        foreach (json_decode($this->getResponse()->getBody(), true)['data'] as $item) {
+            yield new MerchantDto(...$item);
+        }
+    }
+}
