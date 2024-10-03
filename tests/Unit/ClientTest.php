9ba9d23c9b9eddaee97468027232bcdd5fb68c08
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-03 17:57:36 +0400

    test(ClientTest): update test with mock HTTP client
    - Replace `HttpClientInterface` with `MockClient` for testing
    - Use `Psr17Factory` to create mock responses
    - Modify `testSuccessfulRequest` to assert `ResolveResponse` instance
    - Update `composer.lock` by removing `php-http/mock-client` stability flag

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -2,17 +2,38 @@
 
 namespace Muscobytes\TakeAdsApi\Tests\Unit;
 
-use Muscobytes\HttpClient\Interface\HttpClientInterface;
+use Http\Discovery\Psr17Factory;
 use Muscobytes\TakeAdsApi\Client;
-use Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\ResolveRequestDto;
+use Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\Resolve\ResolveRequest;
+use Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\Resolve\ResolveRequestParameters;
+use Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\Resolve\ResolveResponse;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use Http\Mock\Client as MockClient;
 
 #[CoversClass(Client::class)]
 class ClientTest extends TestCase
 {
     public function testSuccessfulRequest()
     {
-        //
+        $mockHttpClient = new MockClient();
+        $apiClient = new Client(
+            'public-key',
+            'http://localhost',
+            $mockHttpClient
+        );
+
+        $factory = new Psr17Factory();
+        $response = $factory->createResponse(200, 'OK')
+            ->withBody($factory->createStream(json_encode(['key' => 'value'])));
+        $mockHttpClient->addResponse($response);
+
+        $resolveRequestParameters = new ResolveRequestParameters([
+            'https://aliexpress.com',
+            'https://temu.com'
+        ]);
+        $resolveRequest = new ResolveRequest($resolveRequestParameters);
+        $response = $apiClient->call($resolveRequest);
+        $this->assertInstanceOf(ResolveResponse::class, $response);
     }
 }
