509937b60d1de9fa831448b52d534ec76393fd44
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-03 13:52:18 +0400

    refactor(dto): make `AffiliateLinkDto` class readonly
    - Change `AffiliateLinkDto` class declaration to readonly to enhance immutability
    - This change ensures that instances of the class cannot be modified after creation

--- src/Dto/V1/Monetize/V2/Resolve/AffiliateLinkDto.php
+++ src/Dto/V1/Monetize/V2/Resolve/AffiliateLinkDto.php
@@ -2,7 +2,7 @@
 
 namespace Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\Resolve;
 
-class AffiliateLinkDto
+readonly class AffiliateLinkDto
 {
     /**
      * @param string $iri Link sent in the request.
