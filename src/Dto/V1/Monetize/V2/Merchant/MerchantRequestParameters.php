cfad343184c09d0bbf42622b70efc199512c084b
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-03 13:13:41 +0400

    feat(merchant): add `MerchantRequestParameters` class
    - Introduce `MerchantRequestParameters` class extending `RequestParameters`
    - Provide properties for pagination and filtering (`next`, `limit`, `updatedAtFrom`, `isActive`)
    - Facilitate retrieval of merchant list through API

---
+++ src/Dto/V1/Monetize/V2/Merchant/MerchantRequestParameters.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Muscobytes\TakeAdsApi\Dto\V1\Monetize\V2\Merchant;
+
+use Muscobytes\TakeAdsApi\Dto\RequestParameters;
+
+/**
+ * Get list of merchants
+ * https://developers.takeads.com/knowledge-base/article/get-list-of-merchants
+ */
+readonly class MerchantRequestParameters extends RequestParameters
+{
+    public function __construct(
+        public ?string $next = null,
+        public ?int $limit = null,
+        public ?string $updatedAtFrom = null,
+        public ?bool $isActive = null
+    )
+    {
+        //
+    }
+}
