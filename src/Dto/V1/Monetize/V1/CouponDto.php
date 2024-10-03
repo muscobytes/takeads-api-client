b846eeb933b5901f29b8d44d2bb7d0dc4e302cae
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-03 11:58:34 +0400

    refactor(dto): update `CouponDto` properties for nullable types and arrays
    - Change `code`, `endDate`, and `description` properties to nullable types to handle optional values
    - Update `countryCodes` and `categoryIds` to arrays for multiple entries
    - Enhance type consistency and improve flexibility for coupon data management

--- src/Dto/V1/Monetize/V1/CouponDto.php
+++ src/Dto/V1/Monetize/V1/CouponDto.php
@@ -13,16 +13,16 @@ class CouponDto
      * @param bool $isActive Flag indicating if a coupon is active
      * @param string $trackingLink Affiliate link (RFC 3986)
      * @param string $name Coupon name
-     * @param string $code Coupon code
+     * @param string|null $code Coupon code
      * @param string $merchantId Merchant unique identifier
      * @param string $imageUri URI to the coupon logo
      * @param array $languageCodes Array of languages supported by the website in ISO 639-1 alpha-2 format.
      *      If the value is empty, the coupon languages are unknown
      * @param string $startDate Date from which the coupon can be applied
-     * @param string $endDate Date after which the coupon will no longer apply
-     * @param string $description Detailed information about the coupon, including usage conditions and limitations
-     * @param string $countryCodes List of country codes in ISO 3166-1 alpha-2 format where the coupon operates
-     * @param int $categoryId List of merchant category identifiers
+     * @param string|null $endDate Date after which the coupon will no longer apply
+     * @param string|null $description Detailed information about the coupon, including usage conditions and limitations
+     * @param array $countryCodes List of country codes in ISO 3166-1 alpha-2 format where the coupon operates
+     * @param array $categoryIds
      * @param string $createdAt Date of the last update of the coupon in ISO 8601: 1988 (E) format
      * @param string $updatedAt Date of the last update of the coupon in ISO 8601: 1988 (E) format
      */
@@ -31,15 +31,15 @@ class CouponDto
         public bool $isActive,
         public string $trackingLink,
         public string $name,
-        public string $code,
+        public ?string $code,
         public string $merchantId,
         public string $imageUri,
         public array $languageCodes,
         public string $startDate,
-        public string $endDate,
-        public string $description,
-        public string $countryCodes,
-        public int $categoryId,
+        public ?string $endDate,
+        public ?string $description,
+        public array $countryCodes,
+        public array $categoryIds,
         public string $createdAt,
         public string $updatedAt,
     )
