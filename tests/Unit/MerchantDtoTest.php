5088c346be35fdf420cf75b44040d4a06d9e0059
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-07 20:28:28 +0400

    test(MerchantDtoTest): reverse parameter order in `assertSame` calls
    - Reverse the order of parameters in `assertSame` to match expected vs actual convention
    - Improve readability and maintainability of test assertions

--- tests/Unit/MerchantDtoTest.php
+++ tests/Unit/MerchantDtoTest.php
@@ -17,61 +17,67 @@ class MerchantDtoTest extends BaseTest
         $this->assertInstanceOf(MerchantDto::class, $dto);
 
         $this->assertIsInt($dto->merchantId);
-        $this->assertSame($dto->merchantId, $merchant['merchantId']);
+        $this->assertSame($merchant['merchantId'], $dto->merchantId);
 
         $this->assertIsString($dto->name);
-        $this->assertSame($dto->name, $merchant['name']);
+        $this->assertSame($merchant['name'], $dto->name);
 
-        $this->assertSame($dto->imageUri, $merchant['imageUri']);
+        $this->assertSame($merchant['imageUri'], $dto->imageUri);
 
         $this->assertIsString($dto->currencyCode);
-        $this->assertSame($dto->currencyCode, $merchant['currencyCode']);
+        $this->assertSame($merchant['currencyCode'], $dto->currencyCode);
 
         $this->assertIsString($dto->defaultDomain);
-        $this->assertSame($dto->defaultDomain, $merchant['defaultDomain']);
+        $this->assertSame($merchant['defaultDomain'], $dto->defaultDomain);
 
         $this->assertIsArray($dto->domains);
-        $this->assertSame($dto->domains, $merchant['domains']);
+        $this->assertSame($merchant['domains'], $dto->domains);
 
-        $this->assertSame($dto->categoryId, $merchant['categoryId']);
+        $this->assertSame($merchant['categoryId'], $dto->categoryId);
 
-        $this->assertSame($dto->description, $merchant['description']);
+        $this->assertSame($merchant['description'], $dto->description);
 
         $this->assertIsBool($dto->isActive);
-        $this->assertSame($dto->isActive, $merchant['isActive']);
+        $this->assertSame($merchant['isActive'], $dto->isActive);
 
         $this->assertIsArray($dto->countryCodes);
-        $this->assertSame($dto->countryCodes, $merchant['countryCodes']);
+        $this->assertSame($merchant['countryCodes'], $dto->countryCodes);
 
         is_null($merchant['averageBasketValue']) ?
             $this->assertNull($dto->averageBasketValue) : $this->assertIsFloat($dto->averageBasketValue);
+        $this->assertSame($merchant['averageBasketValue'], $dto->averageBasketValue);
 
         is_null($merchant['averageCommission']) ?
             $this->assertNull($dto->averageCommission) : $this->assertIsFloat($dto->averageCommission);
+        $this->assertSame($merchant['averageCommission'], $dto->averageCommission);
 
         is_null($merchant['averageConfirmationTime']) ?
             $this->assertNull($dto->averageConfirmationTime) : $this->assertIsFloat($dto->averageConfirmationTime);
+        $this->assertSame($merchant['averageConfirmationTime'], $dto->averageConfirmationTime);
 
         is_null($merchant['averageCancellationRate']) ?
             $this->assertNull($dto->averageCancellationRate) : $this->assertIsFloat($dto->averageCancellationRate);
+        $this->assertSame($merchant['averageCancellationRate'], $dto->averageCancellationRate);
 
         is_null($merchant['minimumCommission']) ?
             $this->assertNull($dto->minimumCommission) : $this->assertIsFloat($dto->minimumCommission);
+        $this->assertSame($merchant['minimumCommission'], $dto->minimumCommission);
 
         is_null($merchant['maximumCommission']) ?
             $this->assertNull($dto->maximumCommission) : $this->assertIsFloat($dto->maximumCommission);
+        $this->assertSame($merchant['maximumCommission'], $dto->maximumCommission);
 
         $this->assertIsArray($dto->commissionRates);
-        $this->assertSame($dto->commissionRates, $merchant['commissionRates']);
+        $this->assertSame($merchant['commissionRates'], $dto->commissionRates);
 
         $this->assertIsString($dto->trackingLink);
-        $this->assertSame($dto->trackingLink, $merchant['trackingLink']);
+        $this->assertSame($merchant['trackingLink'], $dto->trackingLink);
 
         $this->assertIsString($dto->createdAt);
-        $this->assertSame($dto->createdAt, $merchant['createdAt']);
+        $this->assertSame($merchant['createdAt'], $dto->createdAt);
 
         $this->assertIsString($dto->updatedAt);
-        $this->assertSame($dto->updatedAt, $merchant['updatedAt']);
+        $this->assertSame($merchant['updatedAt'], $dto->updatedAt);
     }
 
 
