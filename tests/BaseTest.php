241522294314934212d4a1a0c09085c2ccade6ef
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-07 21:14:43 +0400

    test(BaseTest): add basic test to ensure true is true
    - Add `testIfTrue` method to `BaseTest` class
    - Use `assertTrue` to verify basic truthiness
    - Annotate class with `CoversClass` attribute to specify the covered class

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,9 +2,14 @@
 
 namespace Muscobytes\TakeAdsApi\Tests;
 
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(BaseTest::class)]
 class BaseTest extends TestCase
 {
-
+    public function testIfTrue()
+    {
+        $this->assertTrue(true);
+    }
 }
