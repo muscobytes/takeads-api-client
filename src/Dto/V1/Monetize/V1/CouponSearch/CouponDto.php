838094d0404af13cf316efe51c10ed5b69204cb9
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-08 14:46:12 +0400

    refactor(coupon-dto): update date parameters to use DateTimeInterface
    - Change `$startDate` and `$endDate` types from `string` to `DateTimeInterface` to ensure proper date handling
    - Adjust `$createdAt` and `$updatedAt` types from `string` to `DateTimeInterface` for consistency and accuracy in date representation

--- src/Dto/V1/Monetize/V1/CouponSearch/CouponDto.php
+++ src/Dto/V1/Monetize/V1/CouponSearch/CouponDto.php
@@ -23,12 +23,12 @@ final readonly class CouponDto
      * @param array $languageCodes Array of languages supported by the website in ISO 639-1 alpha-2 format.
      *      If the value is empty, the coupon languages are unknown.
      * @param array $countryCodes Date from which the coupon can be applied.
-     * @param string $startDate Date after which the coupon will no longer apply.
-     * @param string $endDate Detailed information about the coupon, including usage conditions and limitations.
+     * @param DateTimeInterface $startDate Date after which the coupon will no longer apply.
+     * @param DateTimeInterface $endDate Detailed information about the coupon, including usage conditions and limitations.
      * @param string $description List of the country codes in ISO 3166-1 alpha-2 format where the coupon operates.
      * @param array $categoryIds List of merchant category identifiers.
-     * @param string $createdAt Date of the last update of the coupon in ISO 8601: 1988 (E) format.
-     * @param string $updatedAt Date of the last update of the coupon in ISO 8601: 1988 (E) format.
+     * @param DateTimeInterface $createdAt Date of the last update of the coupon in ISO 8601: 1988 (E) format.
+     * @param DateTimeInterface $updatedAt Date of the last update of the coupon in ISO 8601: 1988 (E) format.
      */
     public function __construct(
         public string $couponId,
