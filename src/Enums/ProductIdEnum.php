016ae4e61922f764c2986ce0f7a5f78f79b636af
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-09 21:27:58 +0400

    refactor(enums): specify backing type for `ProductIdEnum`
    - Define `ProductIdEnum` with a backing type of `string`
    - Assign string values to each case for clearer and more explicit usage

--- src/Enums/ProductIdEnum.php
+++ src/Enums/ProductIdEnum.php
@@ -2,11 +2,11 @@
 
 namespace Muscobytes\TakeAdsApi\Enums;
 
-enum ProductIdEnum
+enum ProductIdEnum: string
 {
-    case MONETIZE_LINK_SCRIPT;
-    case MONETIZE_LINK_API;
-    case MONETIZE_API;
-    case MONETIZE_SEARCH_API;
-    case MONETIZE_SUGGEST;
+    case MONETIZE_LINK_SCRIPT = 'MONETIZE_LINK_SCRIPT';
+    case MONETIZE_LINK_API = 'MONETIZE_LINK_API';
+    case MONETIZE_API = 'MONETIZE_API';
+    case MONETIZE_SEARCH_API = 'MONETIZE_SEARCH_API';
+    case MONETIZE_SUGGEST = 'MONETIZE_SUGGEST';
 }
