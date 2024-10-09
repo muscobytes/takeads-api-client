b9ff935ef6e5dc77c54e43b9196533dd8578ed62
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-09 12:29:27 +0400

    refactor(api): simplify data retrieval in `ClickResponse`
    - Replace `foreach` loop with `array_map` for creating `ClickDto` instances
    - Improve code readability and maintainability by using a functional approach

--- src/Dto/V1/Api/Stats/Click/ClickResponse.php
+++ src/Dto/V1/Api/Stats/Click/ClickResponse.php
@@ -16,8 +16,9 @@ final class ClickResponse extends Response
 
     public function getData(): Generator
     {
-        foreach (json_decode($this->getResponse()->getBody(), true)['data'] as $item) {
-            yield new ClickDto(...$item);
-        }
+        yield array_map(
+            fn (array $item) => new ClickDto(...$item),
+            json_decode($this->getResponse()->getBody(), true)['data']
+        );
     }
 }
