1f59032aa8b72d9af290c37b38b8322f139d1545
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-09 10:36:06 +0400

    feat(merchant): adds PHPDoc for `_construct` method of MerchantRequestParameters class

--- src/Dto/V1/Monetize/V2/Merchant/MerchantRequestParameters.php
+++ src/Dto/V1/Monetize/V2/Merchant/MerchantRequestParameters.php
@@ -10,6 +10,17 @@ use Muscobytes\TakeAdsApi\Dto\RequestParameters;
  */
 final readonly class MerchantRequestParameters extends RequestParameters
 {
+    /**
+     * @param string|null $next Pointer (integer) to the next page of the merchant list. If specified, a list
+     *      of merchants from the indicated page is retrieved.
+     * @param int|null $limit Maximum number of merchants that may be returned for a single request.
+     *      The maximum value is 500. The default value is 100.
+     * @param string|null $updatedAtFrom Lower border of the updatedAt field to filter (included). It specifies
+     *      the starting point of the period during which information about merchants was last updated. Date and time
+     *      in the ISO 8601 format.
+     *      Example: Example: 2021-08-03T19:53:15.816Z
+     * @param bool|null $isActive Status of the merchant. A merchant is active if it has at least one active program.
+     */
     public function __construct(
         public ?string $next = null,
         public ?int $limit = null,
