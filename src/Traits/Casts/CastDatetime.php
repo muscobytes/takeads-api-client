7356ea1111b3711411229ac73f9e25a9b845d31f
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-13 20:49:32 +0400

    fix(casts): correct constant declaration in `CastDatetime`
    - Remove redundant `string` type declaration from `FORMAT_ISO_8601` constant
    - Ensure the constant is correctly defined to avoid potential errors in datetime casting

--- src/Traits/Casts/CastDatetime.php
+++ src/Traits/Casts/CastDatetime.php
@@ -6,7 +6,7 @@ use \DateTime;
 
 trait CastDatetime
 {
-    const string FORMAT_ISO_8601 = 'Y-m-d\TH:i:s.vp';
+    const FORMAT_ISO_8601 = 'Y-m-d\TH:i:s.vp';
 
     protected static function castDatetime(
         string $timestamp,
