d026813a979a8479b861903b4ad6ceeced7e18b3
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-15 15:47:26 +0400

    refactor(traits): consolidate array transformation logic
    - Combine `removeNullValues` and `transformBoolean` methods into `transformArray` for better maintainability
    - Add support for nested arrays and objects in `transformArray`
    - Update `toArray` method to utilize the new `transformArray` method
    - Introduce `removeNullValues` parameter in `toArray` for optional null value removal

--- src/Traits/ToArray.php
+++ src/Traits/ToArray.php
@@ -4,34 +4,31 @@ namespace Muscobytes\TakeadsApi\Traits;
 
 trait ToArray
 {
-    public function removeNullValues(array $parameters): array
-    {
-        return array_filter(
-            $parameters,
-            fn ($parameter) => !is_null($parameter)
-        );
-    }
-
-
-    /**
-     * Transforms boolean values into string
-     * @param array $array
-     * @return array
-     */
-    public function transformBoolean(array $array): array
+    public function transformArray(
+        array $array,
+        bool $transformBoolean
+    ): array
     {
         return array_map(
-            fn ($value) => is_bool($value) ? $value ? 'true' : 'false' : $value,
+            fn (mixed $element) => match(gettype($element)) {
+                'boolean' => $transformBoolean ? $element ? 'true' : 'false' : $element,
+                'object' => $element->toArray(),
+                'array' => $this->transformArray($element, $transformBoolean),
+                default => $element
+            },
             $array
         );
     }
 
 
-    public function toArray(bool $transformBoolean = false): array
+    public function toArray(
+        bool $transformBoolean = false,
+        bool $removeNullValues = true
+    ): array
     {
-        return $transformBoolean ?
-            $this->transformBoolean(get_object_vars($this)) :
-            $this->removeNullValues(get_object_vars($this))
-            ;
+        return array_filter(
+            $this->transformArray(get_object_vars($this), $transformBoolean),
+            fn ($value) => !$removeNullValues || !is_null($value)
+        );
     }
 }
