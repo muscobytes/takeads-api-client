9dc9eba33becc89c26e06deadb7ce26a4d5ae5a7
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-15 15:47:47 +0400

    test(traits): add unit test for `ToArray` trait
    - Create `ToArrayTest` class to test the `toArray` method
    - Verify the output is an array and contains expected keys
    - Ensure `iris` key is an array and has the correct count
    - Check for the absence of `subid` key
    - Confirm `withImages` key exists and is a boolean with expected value

---
+++ tests/Unit/Traits/ToArrayTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Muscobytes\TakeadsApi\Tests\Unit\Traits;
+
+use Muscobytes\TakeadsApi\Dto\V1\Monetize\V2\Resolve\ResolveRequestParameters;
+use Muscobytes\TakeadsApi\Tests\BaseTest;
+use Muscobytes\TakeadsApi\Traits\ToArray;
+use PHPUnit\Framework\Attributes\CoversMethod;
+
+
+#[CoversMethod(ToArray::class, 'toArray')]
+class ToArrayTest extends BaseTest
+{
+    use ToArray;
+
+
+    public function testToArray()
+    {
+        $dto = new ResolveRequestParameters(
+            iris: ['https://aliexpress.com']
+        );
+        $array = $dto->toArray();
+        $this->assertIsArray($array);
+        $this->assertNotEmpty($array);
+        $this->assertArrayHasKey('iris', $array);
+        $this->assertIsArray($array['iris']);
+        $this->assertCount(1, $array['iris']);
+        $this->assertArrayNotHasKey('subid', $array);
+        $this->assertArrayHasKey('withImages', $array);
+        $this->assertIsBool($array['withImages']);
+        $this->assertSame(false, $array['withImages']);
+    }
+}
