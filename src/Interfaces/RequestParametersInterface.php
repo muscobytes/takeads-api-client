639520a3ec8fe5fd9975b82ec8c1f5428685f18b
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-15 15:48:41 +0400

    refactor(RequestParametersInterface): remove `removeNullValues` method
    - Remove `removeNullValues` method from `RequestParametersInterface`
    - Simplify interface by keeping only the `toArray` method
    - Clean up unnecessary code for better maintainability

--- src/Interfaces/RequestParametersInterface.php
+++ src/Interfaces/RequestParametersInterface.php
@@ -4,7 +4,5 @@ namespace Muscobytes\TakeadsApi\Interfaces;
 
 interface RequestParametersInterface
 {
-    public function removeNullValues(array $parameters): array;
-
     public function toArray(bool $transformBoolean): array;
 }
