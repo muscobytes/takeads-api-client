1ec2ef0edfa7f8b61c68a3788267a24381ef28e5
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-15 12:17:47 +0400

    refactor(dto): extract `toArray` methods into a trait
    - Move `removeNullValues`, `transformBoolean`, and `toArray` methods from `RequestParameters` class to `toArray` trait
    - Improve code reusability and maintainability by using the `toArray` trait in `RequestParameters` class

--- src/Dto/RequestParameters.php
+++ src/Dto/RequestParameters.php
@@ -3,37 +3,9 @@
 namespace Muscobytes\TakeadsApi\Dto;
 
 use Muscobytes\TakeadsApi\Interfaces\RequestParametersInterface;
+use Muscobytes\TakeadsApi\Traits\toArray;
 
 abstract readonly class RequestParameters implements RequestParametersInterface
 {
-    public function removeNullValues(array $parameters): array
-    {
-        return array_filter(
-            $parameters,
-            fn ($parameter) => !is_null($parameter)
-        );
-    }
-
-
-    /**
-     * Transforms boolean values into string
-     * @param array $array
-     * @return array
-     */
-    public function transformBoolean(array $array): array
-    {
-        return array_map(
-            fn ($value) => is_bool($value) ? $value ? 'true' : 'false' : $value,
-            $array
-        );
-    }
-
-
-    public function toArray(bool $transformBoolean = false): array
-    {
-        return $transformBoolean ?
-            $this->transformBoolean(get_object_vars($this)) :
-            $this->removeNullValues(get_object_vars($this))
-            ;
-    }
+    use toArray;
 }
---
+++ src/Traits/toArray.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Muscobytes\TakeadsApi\Traits;
+
+trait toArray
+{
+    public function removeNullValues(array $parameters): array
+    {
+        return array_filter(
+            $parameters,
+            fn ($parameter) => !is_null($parameter)
+        );
+    }
+
+
+    /**
+     * Transforms boolean values into string
+     * @param array $array
+     * @return array
+     */
+    public function transformBoolean(array $array): array
+    {
+        return array_map(
+            fn ($value) => is_bool($value) ? $value ? 'true' : 'false' : $value,
+            $array
+        );
+    }
+
+
+    public function toArray(bool $transformBoolean = false): array
+    {
+        return $transformBoolean ?
+            $this->transformBoolean(get_object_vars($this)) :
+            $this->removeNullValues(get_object_vars($this))
+            ;
+    }
+}
