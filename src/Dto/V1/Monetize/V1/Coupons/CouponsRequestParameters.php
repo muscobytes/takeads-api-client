bcda60033dc1493ad042194faf5114db2615ba7c
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-19 03:19:35 +0400

    feat(coupons): enhance `CouponsRequestParameters` with datetime casting
    - Add `CastDatetime` trait to handle datetime conversion
    - Change date-related properties to `DateTimeInterface` type
    - Introduce `fromArray` method for constructing instances from arrays
    - Implement automatic datetime casting in `fromArray` method

--- src/Dto/V1/Monetize/V1/Coupons/CouponsRequestParameters.php
+++ src/Dto/V1/Monetize/V1/Coupons/CouponsRequestParameters.php
@@ -2,7 +2,9 @@
 
 namespace Muscobytes\TakeadsApi\Dto\V1\Monetize\V1\Coupons;
 
+use DateTimeInterface;
 use Muscobytes\TakeadsApi\Dto\RequestParameters;
+use Muscobytes\TakeadsApi\Traits\Casts\CastDatetime;
 
 /**
  * Get coupons
@@ -10,19 +12,56 @@ use Muscobytes\TakeadsApi\Dto\RequestParameters;
  */
 final class CouponsRequestParameters extends RequestParameters
 {
+    use CastDatetime;
+
     public function __construct(
-        public ?string $isActive = null,
-        public ?string $updatedAtFrom = null,
-        public ?string $updatedAtTo = null,
-        public ?string $startDateBefore = null,
-        public ?string $endDateAfter = null,
-        public ?array $languageCodes = null,
-        public ?array $categoryIds = null,
-        public ?array $countryCodes = null,
-        public ?string $next = null,
-        public ?int $limit = null
+        public ?bool              $isActive = null,
+        public ?DateTimeInterface $updatedAtFrom = null,
+        public ?DateTimeInterface $updatedAtTo = null,
+        public ?DateTimeInterface $startDateBefore = null,
+        public ?DateTimeInterface $endDateAfter = null,
+        public ?array             $languageCodes = null,
+        public ?array             $categoryIds = null,
+        public ?array             $countryCodes = null,
+        public ?string            $next = null,
+        public ?int               $limit = null
     )
     {
         //
+    }
+
+
+    public static function fromArray(array $array): self
+    {
+        $properties = [
+            'isActive',
+            'updatedAtFrom',
+            'updatedAtTo',
+            'startDateBefore',
+            'endDateAfter',
+            'languageCodes',
+            'countryCodes',
+            'next',
+            'limit'
+        ];
+
+        return new self(...array_map(
+            function ($propertyName) use ($array) {
+                $castDateTime = [
+                    'updatedAtFrom',
+                    'updatedAtTo',
+                    'startDateBefore',
+                    'endDateAfter',
+                ];
+                if (key_exists($propertyName, $array)) {
+                    if (in_array($propertyName, $castDateTime)) {
+                        return self::castDatetime($array[$propertyName] . 'T00:00:00.000Z');
+                    }
+                    return $array[$propertyName];
+                }
+                return null;
+            },
+            $properties
+        ));
     }
 }
