84c513b0f316588ae87964c184b933984f3484eb
Stanislav Surskiy <ssurskiy@gmail.com>
2025-01-11 13:16:01 +0400

    fix(dto): allow `next` property to be nullable in `ResponseMetaNext`
    - Change the `next` property type from `string` to `?string` to handle nullable values
    - Ensure compatibility with API responses where `next` might not be provided

--- src/Dto/ResponseMetaNext.php
+++ src/Dto/ResponseMetaNext.php
@@ -5,7 +5,7 @@ namespace Muscobytes\TakeadsApi\Dto;
 final class ResponseMetaNext
 {
     public function __construct(
-        public string $next
+        public ?string $next
     )
     {
         //
